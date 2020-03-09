5b939ffc2a827dfb7b7b5fcee6552cafa13183f1
Danila <permogorsky.danila@gmail.com>
2020-03-09 18:45:55 +0300

    Make a db connect

---
+++ BookStudy/DBConnect/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using BookStudy.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStudy.DBConnect
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<GuestResponse> GuestResponses { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            //create a DB all the same time with connection
+            Database.EnsureCreated();
+        }
+    }
+}
