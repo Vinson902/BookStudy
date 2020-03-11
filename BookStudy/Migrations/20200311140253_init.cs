a2b4a0b25e05c3041e80251d8fa963b05c0d9da4
Danila <permogorsky.danila@gmail.com>
2020-03-11 21:44:49 +0300

    Make a local db connect (MS SQL)

---
+++ BookStudy/Migrations/20200311140253_init.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookStudy.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GuestResponses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(nullable: false),
+                    email = table.Column<string>(nullable: false),
+                    phone = table.Column<string>(nullable: false),
+                    willAttend = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GuestResponses", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GuestResponses");
+        }
+    }
+}
