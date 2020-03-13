e6747d941851c8043ce1d776ee1e39bf8d9a5f14
Danila <permogorsky.danila@gmail.com>
2020-03-13 10:54:52 +0300

    Make a dc connect (Heroku)

--- BookStudy/Startup.cs
+++ BookStudy/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Npgsql;
 
 namespace BookStudy
 {
@@ -26,10 +27,30 @@ namespace BookStudy
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
+           //  //get sql uri
+           //  
+           // // var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+           //  var databaseUri = new Uri("postgres://djyftvpmjxyern:17ea447c417c27ff9c38d93ad8ed5bdb59a5b84a0b1244110ecfb0c588183519@ec2-50-17-178-87.compute-1.amazonaws.com:5432/d5fs07chti6g70");
+           //  var userInfo = databaseUri.UserInfo.Split(':');
+           //
+           //  var builder = new NpgsqlConnectionStringBuilder
+           //  {
+           //      Host = databaseUri.Host,
+           //      Port = databaseUri.Port,
+           //      Username = userInfo[0],
+           //      Password = userInfo[1],
+           //      Database = databaseUri.LocalPath.TrimStart('/'),
+           //      SslMode = SslMode.Require,
+           //      TrustServerCertificate = true
+           //  };
+           //
+           //  string conn = builder.ToString();
             //get connection string from configure file
-            string connection = Configuration.GetConnectionString("DefaultConnection");
+            string connection = Configuration.GetConnectionString("TestConnection");
             services.AddDbContext<ApplicationContext>(options =>
-                options.UseSqlServer(connection));
+                // options.UseSqlServer(connection)
+                options.UseNpgsql(connection));
             services.AddControllersWithViews();
             
         }
