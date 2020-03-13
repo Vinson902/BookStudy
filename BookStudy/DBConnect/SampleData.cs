b7ff2fe01fa3b4fb7b39ca635ca4acece0242d62
Danila <permogorsky.danila@gmail.com>
2020-03-13 14:35:12 +0300

    Make a Sample Data for db

---
+++ BookStudy/DBConnect/SampleData.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using BookStudy.Models;
+
+namespace BookStudy.DBConnect
+{
+    public class SampleData
+    {
+        public static void Initialize(ApplicationContext context)
+        {
+                context.GuestResponses.AddRange(
+                    new GuestResponse
+                    {
+                        name = "Timofey",
+                        email = "timofey@gmail.com",
+                        phone = "89763456734"
+                    },
+                new GuestResponse
+                    {
+                        name = "Danila",
+                        email = "danila@gmail.com",
+                        phone = "79021906946"
+                    },
+                new GuestResponse
+                    {
+                        name = "Anybody",
+                        email = "timofey@gmail.com",
+                        phone = "0123456789"
+                    }
+                    );
+                context.SaveChanges();
+        }
+    }
+}
